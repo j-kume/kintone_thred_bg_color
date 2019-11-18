03b8b908daf75c1c4bdfd38a3d2cc5f58d96856c
Junya Kume <j-kume@aisic.jp>
2019-11-18 15:00:34 +0900

    first commit

---
+++ theread_bg_color_m.js
@@ -0,0 +1,22 @@
+//=============================================================================
+//【ファイル名】
+//    thread_bg_color_m.js
+//【アプリ名】
+//    スレッドに背景色を付ける
+//【タイプ】
+//    ○PC用 / ●モバイル用
+//-----------------------------------------------------------------------------
+//  Copyright (c) 2019 AISIC Inc.
+//=============================================================================
+
+(function () {
+    'use strict';
+    const parent_color = '#E0F8EC';
+    const child_color = '#EFFBF5';
+    setInterval(function(){
+        document.querySelectorAll('.ocean-ui-comments-commentbase-entity').forEach(val=>val.style.backgroundColor = parent_color);
+        document.querySelectorAll('.ocean-ui-comments-post-commentholder .ocean-ui-comments-commentbase-entity ').forEach(val=>val.style.backgroundColor = child_color);
+    }, 1000);
+})();
+
+
